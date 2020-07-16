1f19e33405840b49fcb3447c738b30bea1b18f5d
dnrlwo <dnrlwo11@gmail.com>
2020-07-16 10:36:14 +0900

    add down arrow

--- components/ProfileAvatar.js
+++ components/ProfileAvatar.js
@@ -1,14 +1,15 @@
 import React, { useCallback } from 'react';
 import { useSelector, useDispatch} from 'react-redux';
 import Link from 'next/link';
-import { Popover, List, Avatar, Descriptions, Tabs, Button, message, Card } from 'antd';
-import { SettingOutlined, RightOutlined, DeleteOutlined } from '@ant-design/icons';
+import { Popover, List, Avatar, Descriptions, Tabs, Button, message, Card, Space } from 'antd';
+import { SettingOutlined, RightOutlined, DeleteOutlined, DownOutlined } from '@ant-design/icons';
 import  Router  from 'next/router';
 import { LOG_OUT_REQUEST } from '../reducers/user';
 
 
 const ProfileAvatar = () => {
     const { me } = useSelector(state => state.user, (left, right)=>{ if (left['me']['id'] == right['me']['id']){return true}else{return false}});
+    const isLoggingOut = useSelector(state => state.user.isLoggingOut);
     const dispatch = useDispatch();
     const onLogOut = useCallback(() => {
         dispatch({type: LOG_OUT_REQUEST});
@@ -16,48 +17,51 @@ const ProfileAvatar = () => {
     },[]);
 
     return (
-        <Popover 
-            placement="rightTop" 
-            trigger="click" 
-            content={
-                <Tabs type="card" size="small" defaultActiveKey="2" style={{width:'250px'}}>
-                    <Tabs.TabPane tab="최근 본 구장" key="1">
-                        <List
-                            footer={<Button size="small" shape="round" onClick={()=>alert("삭제")} block><DeleteOutlined/>최근 본 구장 삭제하기</Button>}
-                            // itemLayout="vertical"
-                            pagination={{pageSize:3}}
-                            dataSource={[{title:"누상동 다목적 운동장", address:"서울-종로", src:''},{title:"서대문 돌산구장", address:"서울-서대문", src:''}, ]}
-                            renderItem={item=>(
-                                <List.Item>
-                                    <Card style={{width:'100%'}} size="small">
-                                        <Card.Meta
-                                            avatar={<Avatar src={item.src ?? 'https://www.americaskidsinmotion.com/wp-content/uploads/2016/05/product-soccer-ball.jpg'}/>}
-                                            title={item.title}
-                                            description={item.address}
-                                        />
-                                    </Card>
-                                </List.Item>
-                            )}
-                        />
-                    </Tabs.TabPane>
-                    <Tabs.TabPane tab="회원정보" key="2">
-                        <Descriptions layout="horizontal" column={1} style={{width:'200px'}} colon={false} >
-                            <Descriptions.Item label="닉네임">{me.nickname}</Descriptions.Item>
-                            <Descriptions.Item label="아이디">{me.id}</Descriptions.Item>
-                            {me.Team.club && <Descriptions.Item label="팀">{me.Team.clubname}   <Link href={`/team/${me.Team.club}`}><a><SettingOutlined/></a></Link></Descriptions.Item> }
-                        </Descriptions>
-                        <Button size="default" block style={{borderRadius:'5px'}} onClick={onLogOut}>로그아웃</Button>
-                    </Tabs.TabPane>
-                    <Tabs.TabPane tab="계정관리" key="3">
-                        <Button block style={{border:0, borderBottom:'1px solid #dadce0'}} onClick={()=>Router.push(`/user/profile`)}>회원정보 수정<RightOutlined style={{float:'right',lineHeight:'25px'}}/></Button>
-                        <Button block style={{border:0, borderBottom:'1px solid #dadce0'}} onClick={()=> Router.push(`/user/altpwd`)}>비밀번호 변경<RightOutlined style={{float:'right',lineHeight:'25px'}}/></Button>
-                        <Button block style={{border:0, borderBottom:'1px solid #dadce0'}}>사진 변경<RightOutlined style={{float:'right',lineHeight:'25px'}}/></Button>
-                    </Tabs.TabPane>
-                </Tabs>
-            } 
-        >
+        <Space>
             <Avatar shape="circle" >{me.nickname}</Avatar>
-        </Popover>
+            <Popover 
+                placement="rightTop" 
+                trigger="click" 
+                content={
+                    <Tabs type="card" size="small" defaultActiveKey="2" style={{width:'250px'}}>
+                        <Tabs.TabPane tab="최근 본 구장" key="1">
+                            <List
+                                footer={<Button size="small" shape="round" onClick={()=>alert("삭제")} block><DeleteOutlined/>최근 본 구장 삭제하기</Button>}
+                                // itemLayout="vertical"
+                                pagination={{pageSize:3}}
+                                dataSource={[{title:"누상동 다목적 운동장", address:"서울-종로", src:''},{title:"서대문 돌산구장", address:"서울-서대문", src:''}, ]}
+                                renderItem={item=>(
+                                    <List.Item>
+                                        <Card style={{width:'100%'}} size="small">
+                                            <Card.Meta
+                                                avatar={<Avatar src={item.src ?? 'https://www.americaskidsinmotion.com/wp-content/uploads/2016/05/product-soccer-ball.jpg'}/>}
+                                                title={item.title}
+                                                description={item.address}
+                                            />
+                                        </Card>
+                                    </List.Item>
+                                )}
+                            />
+                        </Tabs.TabPane>
+                        <Tabs.TabPane tab="회원정보" key="2">
+                            <Descriptions layout="horizontal" column={1} style={{width:'200px'}} colon={false} >
+                                <Descriptions.Item label="닉네임">{me.nickname}</Descriptions.Item>
+                                <Descriptions.Item label="아이디">{me.id}</Descriptions.Item>
+                                {me.Team.club && <Descriptions.Item label="팀">{me.Team.clubname}   <Link href={`/team/${me.Team.club}`}><a><SettingOutlined/></a></Link></Descriptions.Item> }
+                            </Descriptions>
+                            <Button size="default" block style={{borderRadius:'5px'}} onClick={onLogOut} loading={isLoggingOut}>로그아웃</Button>
+                        </Tabs.TabPane>
+                        <Tabs.TabPane tab="계정관리" key="3">
+                            <Button block style={{border:0, borderBottom:'1px solid #dadce0'}} onClick={()=>Router.push(`/user/profile`)}>회원정보 수정<RightOutlined style={{float:'right',lineHeight:'25px'}}/></Button>
+                            <Button block style={{border:0, borderBottom:'1px solid #dadce0'}} onClick={()=> Router.push(`/user/altpwd`)}>비밀번호 변경<RightOutlined style={{float:'right',lineHeight:'25px'}}/></Button>
+                            <Button block style={{border:0, borderBottom:'1px solid #dadce0'}}>사진 변경<RightOutlined style={{float:'right',lineHeight:'25px'}}/></Button>
+                        </Tabs.TabPane>
+                    </Tabs>
+                } 
+            >
+                <DownOutlined />
+            </Popover>
+        </Space>
     );
 }
 
