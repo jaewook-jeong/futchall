ceb5fff8a284c42849b7e4679b5ee5deb2b08b99
dnrlwo <dnrlwo11@gmail.com>
2020-07-21 21:26:39 +0900

    upgrade: can use in web feed viewer

--- components/HeaderMenu.js
+++ components/HeaderMenu.js
@@ -1,21 +1,35 @@
-import React, { useMemo } from 'react';
+import React, { useMemo, useCallback } from 'react';
 import { useSelector, shallowEqual } from 'react-redux';
 import Router from 'next/router';
 import PropTypes from 'prop-types';
-import { HomeOutlined, KeyOutlined } from '@ant-design/icons';
+import { KeyOutlined, HomeOutlined, TeamOutlined } from '@ant-design/icons';
+import { Popover } from 'antd';
 
 import ProfileAvatar from './ProfileAvatar';
 import styles from '../SCSS/headerMenu.module.scss';
 import HeaderSearchBox from './HeaderSearchBox';
 
 const HeaderMenu = (props) => {
-  const { showModal } = props;
-  const { isLoggedIn } = useSelector((state) => state.user, shallowEqual);
+  const { showModal, shrink } = props;
+  const { isLoggedIn, me } = useSelector((state) => state.user, shallowEqual);
   const floatRight = useMemo(() => ({ float: 'right' }), []);
+  const HomeButton = useCallback(() => {
+    Router.push('/');
+  }, []);
+  const StadiaButton = useCallback(() => {
+    Router.push('/stadia');
+  }, []);
+  const TeamButton = useCallback(() => {
+    if (me?.Team.club) {
+      Router.push('/team/[id]', `/team/${me.Team.club}`);
+    } else {
+      Router.push('/team/register');
+    }
+  }, [me]);
   return (
     <div className={styles.headerMenu}>
       <ul>
-        <li onClick={() => Router.push("/")}>
+        <li onClick={HomeButton}>
           <span className={styles.icon}>
             <img src="/futchall.png" alt="futchall title" />
           </span>
@@ -23,6 +37,31 @@ const HeaderMenu = (props) => {
         <li>
           <HeaderSearchBox />
         </li>
+        {shrink
+          && (
+            <>
+              <li>
+                <span className={styles.icon} onClick={StadiaButton}>
+                  <Popover
+                    placement="bottom"
+                    content="구장찾기"
+                  >
+                    <HomeOutlined />
+                  </Popover>
+                </span>
+              </li>
+              <li>
+                <span className={styles.icon} onClick={TeamButton}>
+                  <Popover
+                    placement="bottom"
+                    content={me.Team.club ? '팀관리' : '팀등록'}
+                  >
+                    <TeamOutlined />
+                  </Popover>
+                </span>
+              </li>
+            </>
+          )}
         {!isLoggedIn
           && (
           <li onClick={showModal} style={floatRight}>
@@ -48,6 +87,7 @@ const HeaderMenu = (props) => {
 };
 HeaderMenu.propTypes = {
   showModal: PropTypes.func.isRequired,
+  shrink: PropTypes.bool.isRequired,
 };
 
 export default HeaderMenu;
