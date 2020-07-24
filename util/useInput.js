54f23e28176a915a196eb802d93287386b7d8e55
dnrlwo <dnrlwo11@gmail.com>
2020-07-24 11:30:53 +0900

    update: make hooks directory

--- util/useInput.js
+++
@@ -1,8 +0,0 @@
-import { useState, useCallback } from 'react';
-export const useInput = (initValue = null) => {
-    const [value, setter] = useState(initValue);
-    const handler = useCallback((e) => {
-        setter(e.target.value);
-    }, [value]);
-    return [value, handler];
-};
--- util/useSetter.js
+++
@@ -1,8 +0,0 @@
-import { useState, useCallback } from 'react';
-export const useSetter = (initValue = null) => {
-    const [value, setter] = useState(initValue);
-    const handler = useCallback((e) => {
-        setter(e);
-    }, []);
-    return [value, handler];
-}
