42d82f509f952bc4c06ddefb9f77a71646bf1be9
dnrlwo <dnrlwo11@gmail.com>
2020-08-02 21:42:20 +0900

    fix: stadiumpage, to PostSystem

--- pages/stadium/[id].js
+++ pages/stadium/[id].js
@@ -2,22 +2,27 @@
 import React, { useEffect, useCallback } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import Router, { useRouter } from 'next/router';
-import { Skeleton, Col, Row, Tabs, Button, message, Descriptions, Tag, Typography, Tooltip } from 'antd';
+import { Skeleton, Col, Row, Tabs, Button, message, Descriptions, Tag, Typography, Tooltip, Card } from 'antd';
 import { CopyOutlined, QuestionCircleOutlined } from '@ant-design/icons';
 
 import StadiumComment from '../../components/StadiumComment';
 import { SELECT_STADIUM_REQUEST } from '../../reducers/stadium';
-import styles from '../../SCSS/stadium.module.scss';
-import AppLayout from '../../components/AppLayout';
+import stadiumMapStyles from '../../SCSS/stadium.module.scss';
+import AppLayout2 from '../../components/AppLayout2';
+import Feed from '../../components/Feed';
+import style from '../../SCSS/feedLayout.module.scss';
 
 const Stadium = () => {
   const router = useRouter();
   const dispatch = useDispatch();
   const { id } = router.query;
   const { info, isSelected } = useSelector((state) => state.stadium, (left, right) => { if (left.info.req === right.info.req) { return true; } return false; });
+  const { me, isLoggedIn } = useSelector((state) => state.user, (left, right) => { if (left.me.id === right.me.id) { return true; } return false; });
+
   const moveToTeam = useCallback(() => {
     Router.push(`/team/${info.teamInfo}`);
   }, [info]);
+
   useEffect(
     () => {
       dispatch({ type: SELECT_STADIUM_REQUEST, data: { req: id } });
@@ -38,12 +43,12 @@ const Stadium = () => {
           position: new kakao.maps.LatLng(info.lat, info.lng),
         },
       );
-      const overlayFrame = `<div class=${styles.overlaybox}>
-                <div class=${styles.boxtitle} id="whatShouldIDo">구장 점령 팀</div>
-                <div class=${styles.first} >
+      const overlayFrame = `<div class=${stadiumMapStyles.overlaybox}>
+                <div class=${stadiumMapStyles.boxtitle} id="whatShouldIDo">구장 점령 팀</div>
+                <div class=${stadiumMapStyles.first} >
                     <img src=${info.teamImg}>
-                    <div class=${styles.triangle}>1</div>
-                    <div class=${styles.movietitle}>${info.team}</div>
+                    <div class=${stadiumMapStyles.triangle}>1</div>
+                    <div class=${stadiumMapStyles.movietitle}>${info.team}</div>
                 </div>
             </div>`;
       const customOverlay = new kakao.maps.CustomOverlay({
@@ -60,66 +65,96 @@ const Stadium = () => {
   }, [isSelected]);
 
   return (
-    <AppLayout>
-      <Row gutter={[0, 16]}>
-        <Col xs={{ span: 22, offset: 1 }} sm={{ span: 22, offset: 1 }} md={{ span: 20, offset: 2 }}>
-          <Typography.Title level={3}>
-            <Skeleton loading={!isSelected} active paragraph={false} />
-            {isSelected && info.title}
-            {isSelected && <Button onClick={() => { message.info('링크가 복사되었습니다.'); }} type="link"><CopyOutlined />Copy</Button>}
-          </Typography.Title>
-          <Tabs tabBarExtraContent={<Button onClick={() => { message.warn('준비중입니다.'); }} shape="round"><QuestionCircleOutlined />정보수정</Button>}>
-            <Tabs.TabPane tab="상세정보" key="1">
-              <Descriptions
-                column={{ xxl: 4, xl: 3, lg: 3, md: 2, sm: 1, xs: 1 }}
-                bordered
-                size="small"
+    <AppLayout2>
+      <Row style={{ marginBottom: '10px' }}>
+        <Col className={style.mainInfo}>
+          <Card
+            cover={(
+              <div
+                style={{ width: '100%', backgroundColor: '#ccc', opacity: '0.3' }}
               >
-                <Descriptions.Item label="주소">
+                <img
+                alt="Main image of Stadium"
+                src="https://via.placeholder.com/500x200/808080"
+                style={{ maxHeight: '100%', maxWidth: '100%', width: 'auto', height: '100%', margin: '0 auto' }}
+                />
+              </div>
+            )}
+            className={style.cardDiv}
+          >
+            <Card.Meta
+              title={(
+                <Typography.Title level={3} copyable={isSelected && { text: window.location.pathname }}>
                   <Skeleton loading={!isSelected} active paragraph={false} />
-                  {isSelected && info.address}
-                </Descriptions.Item>
-                <Descriptions.Item label="사용시간">
-                  <Skeleton loading={!isSelected} active paragraph={false} />
-                  {isSelected && info.time}
-                </Descriptions.Item>
-                <Descriptions.Item label="라이트 여부">
-                  <Skeleton loading={!isSelected} active paragraph={false} />
-                  {isSelected && info.light}
-                </Descriptions.Item>
-                <Descriptions.Item label="특징">
-                  <Skeleton loading={!isSelected} active paragraph={false} />
-                  {isSelected && info.special.map((c) => (<Tag key={c}>#{c}</Tag>))}
-                </Descriptions.Item>
-                <Descriptions.Item label="소개" span={2}>
-                  <Skeleton loading={!isSelected} active paragraph={false} />
-                  {isSelected && <Typography.Paragraph ellipsis={{ rows: 1, expandable: true, symbol: <span>더보기</span> }}>{info.description}</Typography.Paragraph>}
-                </Descriptions.Item>
-                <Descriptions.Item label="점령 팀">
-                  <Skeleton loading={!isSelected} active paragraph={false} />
-                  {isSelected && <a onClick={moveToTeam}>{info.team}</a>}
-                </Descriptions.Item>
-                <Descriptions.Item label={<>유효기간 <Tooltip title="점령 후 도전을 받지 않을 시 유지되는 기간입니다."><QuestionCircleOutlined /></Tooltip></>}>
-                  <Skeleton loading={!isSelected} active paragraph={false} />
-                  {isSelected && info.valid}
-                </Descriptions.Item>
-              </Descriptions>
-              <div id="stadiumAddress" style={{ width: '100%', height: '70vh', marginTop: '10px' }} />
-            </Tabs.TabPane>
-            <Tabs.TabPane tab="후기" key="2">
-              <Skeleton active loading={!isSelected} />
-              {isSelected && <StadiumComment />}
-            </Tabs.TabPane>
-            <Tabs.TabPane tab="사진" key="3">
-              <Skeleton active />
-            </Tabs.TabPane>
-          </Tabs>
+                  {isSelected && info.title}
+                  {isSelected && <Button onClick={() => { message.info('링크가 복사되었습니다.'); }} type="link"><CopyOutlined />Copy</Button>}
+                </Typography.Title>
+                )}
+              description={isSelected && info.description}
+              className={style.stickyTitle}
+            />
+          </Card>
         </Col>
       </Row>
-      <Row>
-        <Col xs={{ span: 22, offset: 1 }} sm={{ span: 22, offset: 1 }} md={{ span: 18, offset: 3 }} />
+      <Row className={style.flowInfo}>
+        <Col xs={{ span: 24 }} sm={{ span: 24 }} md={{ span: 10 }}>
+          <div className={style.fixedInfo}>
+            <Tabs tabBarExtraContent={<Button onClick={() => { message.warn('준비중입니다.'); }} shape="round"><QuestionCircleOutlined />정보수정</Button>}>
+              <Tabs.TabPane tab="상세정보" key="1">
+                <Descriptions
+                  column={{ xxl: 4, xl: 2, lg: 2, md: 2, sm: 2, xs: 2 }}
+                  bordered
+                  size="small"
+                >
+                  <Descriptions.Item label="주소" span={2}>
+                    <Skeleton loading={!isSelected} active paragraph={false} />
+                    {isSelected && info.address}
+                  </Descriptions.Item>
+                  <Descriptions.Item label="사용시간" span={2}>
+                    <Skeleton loading={!isSelected} active paragraph={false} />
+                    {isSelected && info.time}
+                  </Descriptions.Item>
+                  <Descriptions.Item label="라이트 여부" span={2}>
+                    <Skeleton loading={!isSelected} active paragraph={false} />
+                    {isSelected && info.light}
+                  </Descriptions.Item>
+                  <Descriptions.Item label="특징" span={2}>
+                    <Skeleton loading={!isSelected} active paragraph={false} />
+                    {isSelected && info.special.map((c) => (<Tag key={c}>#{c}</Tag>))}
+                  </Descriptions.Item>
+                  <Descriptions.Item label="점령 팀" span={2}>
+                    <Skeleton loading={!isSelected} active paragraph={false} />
+                    {isSelected && <a onClick={moveToTeam}>{info.team}</a>}
+                  </Descriptions.Item>
+                  <Descriptions.Item label={<>유효기간 <Tooltip title="점령 후 도전을 받지 않을 시 유지되는 기간입니다."><QuestionCircleOutlined /></Tooltip></>} span={2}>
+                    <Skeleton loading={!isSelected} active paragraph={false} />
+                    {isSelected && info.valid}
+                  </Descriptions.Item>
+                </Descriptions>
+                <div className={style.mapContainer}>
+                  <div id="stadiumAddress" className={style.occupyMap} />
+                </div>
+              </Tabs.TabPane>
+              <Tabs.TabPane tab="후기" key="2">
+                <Skeleton active loading={!isSelected} />
+                {isSelected && <StadiumComment />}
+              </Tabs.TabPane>
+              <Tabs.TabPane tab="사진" key="3">
+                <Skeleton active />
+              </Tabs.TabPane>
+            </Tabs>
+          </div>
+        </Col>
+        <Col
+          xs={{ span: 24 }}
+          sm={{ span: 24 }}
+          md={{ span: 13, offset: 1 }}
+          className={style.postWall}
+        >
+          <Feed where="stadium" req={id} />
+        </Col>
       </Row>
-    </AppLayout>
+    </AppLayout2>
   );
 };
 export default Stadium;
