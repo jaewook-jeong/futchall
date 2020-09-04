8a174fb89c7940b39e2919dec62ffee29735328b
dnrlwo <dnrlwo11@gmail.com>
2020-09-04 18:34:28 +0900

    upgrade: antd version 4.1.5 to 4.6.2

--- pages/_app.js
+++ pages/_app.js
@@ -11,7 +11,7 @@ const FutChall = ({ Component }) => (
     <Head>
       <title>FutChall</title>
       <meta charSet="utf-8" />
-      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/antd/4.1.5/antd.min.css" />
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/antd/4.6.2/antd.min.css" />
       <script type="text/javascript" src="//dapi.kakao.com/v2/maps/sdk.js?appkey=60712c2e792d545d1241f9c9bc3a1f28&libraries=clusterer,services" />
     </Head>
     <Component />
