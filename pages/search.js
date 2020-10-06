3a5dc8b7bde69cad07aaf67cfea323d68fedf16a
dnrlwo <dnrlwo11@gmail.com>
2020-10-06 20:03:29 +0900

    add: show list

--- pages/search.js
+++ pages/search.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
-import { Row, Col, Typography } from 'antd';
+import { Row, Col, Typography, List } from 'antd';
+import moment from 'moment';
 import { END } from 'redux-saga';
 import axios from 'axios';
+import Link from 'next/link';
 
 import { SEARCH_TEAMS_REQUEST } from '../reducers/team';
 import AppLayout from '../components/AppLayout';
@@ -10,13 +12,106 @@ import wrapper from '../store/configureStore';
 import { LOAD_MY_INFO_REQUEST } from '../reducers/user';
 
 const Search = () => {
-  const { teamList, query } = useSelector((state) => state.team);
-  console.log(teamList, query);
+  const { teamList, query, isSearched } = useSelector((state) => state.team);
+
   return (
     <AppLayout>
       <Row>
         <Col xs={{ span: 22, offset: 1 }} md={{ span: 20, offset: 2 }}>
           <Typography.Title level={4}>"{query}"에 대한 검색 결과입니다.</Typography.Title>
+          {
+            isSearched && teamList[0] && (
+            <List
+              header={`게시글 검색 결과 ${teamList[0].length}건`}
+              itemLayout="vertical"
+              size="large"
+              pagination={{ pageSize: 3 }}
+              dataSource={teamList[0]}
+              renderItem={(item) => (
+                <List.Item
+                  key={item.id}
+                  extra={
+                    item.src && (
+                      <img
+                        height="150"
+                        alt="first post image"
+                        src={`http://localhost:3065/${item.src}`}
+                      />
+                    )
+                  }
+                >
+                  <List.Item.Meta
+                    title={<Link href={`/post/${item.id}`}><a>{item.nickname}님의 게시글</a></Link>}
+                    description={moment(item.createdAt.toString()).locale('ko').fromNow()}
+                  />
+                  {item.content}
+                </List.Item>
+              )}
+            />
+            )
+          }
+          {
+            isSearched && teamList[1] && (
+            <List
+              header={`구장 검색 결과 ${teamList[1].length}건`}
+              itemLayout="vertical"
+              size="large"
+              pagination={{ pageSize: 3 }}
+              dataSource={teamList[1]}
+              renderItem={(item) => (
+                <List.Item
+                  key={item.id}
+                  extra={
+                    item.src && (
+                      <img
+                        height="150"
+                        alt="first post image"
+                        src={`http://localhost:3065/${item.src}`}
+                      />
+                    )
+                  }
+                >
+                  <List.Item.Meta
+                    title={<Link href={`/stadium/${item.id}`}><a>{item.title}</a></Link>}
+                    description={item.address}
+                  />
+                  {item.description}
+                </List.Item>
+              )}
+            />
+            )
+          }
+          {
+            isSearched && teamList[2] && (
+            <List
+              header={`팀 검색 결과 ${teamList[2].length}건`}
+              itemLayout="vertical"
+              size="large"
+              pagination={{ pageSize: 3 }}
+              dataSource={teamList[2]}
+              renderItem={(item) => (
+                <List.Item
+                  key={item.id}
+                  extra={
+                    item.src && (
+                      <img
+                        height="150"
+                        alt="first post image"
+                        src={`http://localhost:3065/${item.src}`}
+                      />
+                    )
+                  }
+                >
+                  <List.Item.Meta
+                    title={<Link href={`/team/${item.id}`}><a>{item.title}</a></Link>}
+                    description={item.location}
+                  />
+                  {item.description}
+                </List.Item>
+              )}
+            />
+            )
+          }
         </Col>
       </Row>
     </AppLayout>
