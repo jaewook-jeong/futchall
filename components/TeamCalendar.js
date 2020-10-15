5ee19da07244dba64608bcec2d60586d21df6f7b
dnrlwo <dnrlwo11@gmail.com>
2020-10-15 19:00:15 +0900

    fix: teamCalendar

--- components/TeamCalendar.js
+++ components/TeamCalendar.js
@@ -1,5 +1,5 @@
-import React, { useCallback, useEffect } from 'react';
-import { Button, Calendar, Col, Drawer, Popconfirm, Row, Select } from 'antd';
+import React, { useCallback, useEffect, useMemo, useState } from 'react';
+import { Button, Calendar, Col, Drawer, Popconfirm, Row, Select, Tooltip } from 'antd';
 import moment from 'moment';
 import PropTypes from 'prop-types';
 import { useDispatch, useSelector } from 'react-redux';
@@ -8,38 +8,80 @@ import { CheckSquareOutlined } from '@ant-design/icons';
 import { GET_CALENDAR_REQUEST } from '../reducers/team';
 
 const TeamCalendar = ({ setVisible, teamId, visible }) => {
-  const { calendar } = useSelector((state) => state.team);
+  const { calendar, isGettedCalendar } = useSelector((state) => state.team);
+  const [yearMonth, setYearMonth] = useState(moment().format('YYYY-MM-01'));
   const dispatch = useDispatch();
   const onClose = useCallback(() => {
     setVisible(false);
   }, []);
+  const SortingData = (list) => {
+    const sortedData = {};
+    if (isGettedCalendar) {
+      list.forEach((value) => {
+        if (sortedData[value.date]) {
+          sortedData[value.date].push(value);
+        } else {
+          sortedData[value.date] = [value];
+        }
+      });
+    }
+    return sortedData;
+  };
+  const sortedDate = useMemo(() => SortingData(calendar), [calendar]);
   const dateCellRender = useCallback((value) => {
-    console.log(calendar);
+    const data = sortedDate[value.format('YYYY-MM-DD')];
     return (
-      null
-      // <Popconfirm
-      // title="참석하시겠습니까?"
-      // okText="참석"
-      // cancelText="취소"
-      // >
-      //   <CheckSquareOutlined />
-      // </Popconfirm>
+      <Row>
+        <Col span={24}>
+          <Popconfirm
+            title="참석하시겠습니까?"
+            okText="참석"
+            cancelText="취소"
+          >
+            <CheckSquareOutlined />
+          </Popconfirm>
+        </Col>
+        <Col span={24}>
+          {
+            data && (
+              data.filter((v) => v.possible === '0').length
+            )
+          }
+        </Col>
+        <Col span={24}>
+          {
+            data && (
+              data.filter((v) => v.possible === '1').length
+            )
+          }
+        </Col>
+        <Col span={24}>
+          {
+            data && (
+              data.filter((v) => v.possible === '2').length
+            )
+          }
+        </Col>
+      </Row>
     );
-  }, []);
+  }, [sortedDate]);
   const onSelectDate = useCallback((date) => {
-    console.log(date.format('YYYY-MM-DD'));
-  });
+    if (yearMonth !== date.format('YYYY-MM-01')) {
+      setYearMonth(date.format('YYYY-MM-01'));
+    }
+  }, [yearMonth]);
   const disabledDate = useCallback((current) => current && current < moment().endOf('day'), []);
+
   useEffect(() => {
     dispatch({
       type: GET_CALENDAR_REQUEST,
       data: {
         teamId,
-        startDate: moment().startOf('month').format('YYYY-MM-DD'),
-        endDate: moment().endOf('month').format('YYYY-MM-DD'),
+        startDate: yearMonth,
+        endDate: moment(yearMonth, 'YYYY-MM-DD').endOf('month').format('YYYY-MM-DD'),
       },
     });
-  }, []);
+  }, [yearMonth]);
   return (
     <Drawer
       title="스케줄 관리"
