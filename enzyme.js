49f9fa20c1c947f33861b0a87a0ef79d6bf8a2a3
dnrlwo <dnrlwo11@gmail.com>
2020-11-04 22:41:26 +0900

    add: nock config

--- enzyme.js
+++ enzyme.js
@@ -1,3 +1,7 @@
 import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
+import http from 'axios/lib/adapters/http';
+import axios from 'axios';
+
+axios.defaults.adapter = http;
 configure({ adapter: new Adapter() });
