efde116ee7cfaec4ac99f50253f8f8a450c22a2c
dnrlwo <dnrlwo11@gmail.com>
2020-11-04 22:41:45 +0900

    fix: delete default value

--- jest.config.js
+++ jest.config.js
@@ -1,28 +1,8 @@
 module.exports = {
-  moduleFileExtensions: [
-    "ts",
-    "tsx",
-    "js"
-  ],
-  globals: {
-    "ts-jest": {
-      useBabelrc: true,
-      tsConfigFile: "jest.tsconfig.json"
-    }
-  },
   testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
-  coveragePathIgnorePatterns: [
-    "/node_modules/",
-    "enzyme.js"
-  ],
   setupFilesAfterEnv: ["<rootDir>/enzyme.js"],
-  coverageReporters: [
-    "json",
-    "lcov",
-    "text",
-    "text-summary"
-  ],
   moduleNameMapper: {
     "\\.(css|less|scss)$": "identity-obj-proxy"
-  }
+  },
+  snapshotSerializers: ["enzyme-to-json/serializer"]
 };
