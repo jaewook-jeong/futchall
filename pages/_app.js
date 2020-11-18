dbc4a27333be917993e35d239474f40326a1c17f
dnrlwo <dnrlwo11@gmail.com>
2020-11-18 19:21:23 +0900

    add: google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -13,6 +13,13 @@ const FutChall = ({ Component }) => (
       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/antd/4.6.2/antd.min.css" />
       <link rel="icon" type="image/png" href={`/favicon.png`} />
       <script type="text/javascript" src="//dapi.kakao.com/v2/maps/sdk.js?appkey=60712c2e792d545d1241f9c9bc3a1f28&libraries=clusterer,services" />
+      <script async src="https://www.googletagmanager.com/gtag/js?id=G-7P8J27ZSHP"></script>
+      <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', 'G-7P8J27ZSHP');
+      </script>
     </Head>
     <Component />
   </>
