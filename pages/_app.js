1e5fa5cc02ac5193967480552e5c63e6fe71f9d2
dnrlwo <dnrlwo11@gmail.com>
2020-11-18 19:25:31 +0900

    fix: google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -16,7 +16,7 @@ const FutChall = ({ Component }) => (
       <script async src="https://www.googletagmanager.com/gtag/js?id=G-7P8J27ZSHP"></script>
       <script>
         window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
+        function gtag(){dataLayer.push(arguments)}
         gtag('js', new Date());
         gtag('config', 'G-7P8J27ZSHP');
       </script>
