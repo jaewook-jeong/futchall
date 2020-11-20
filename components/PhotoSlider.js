e43bfca91d99689ab0a19004281106860416c3ef
dnrlwo <dnrlwo11@gmail.com>
2020-11-20 18:08:33 +0900

    fix: get original img

--- components/PhotoSlider.js
+++ components/PhotoSlider.js
@@ -31,7 +31,7 @@ const PhotoSlider = ({ images, handler, visible }) => {
           <div key={v.src}>
             <Thumnail>
               <Centered>
-                <ImageTag src={`${v.src}`} alt={`${v.src}`} />
+                <ImageTag src={`${v.src.replace(/\/thumb\//, '/original/')}`} alt={`${v.src}`} />
               </Centered>
             </Thumnail>
           </div>
