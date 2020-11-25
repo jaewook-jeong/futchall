6ec04a9c64f748dd8c9f4500ed6618c04fa5c71f
dnrlwo <dnrlwo11@gmail.com>
2020-11-25 17:12:14 +0900

    test

--- pages/team/ranking.js
+++ pages/team/ranking.js
@@ -52,31 +52,31 @@ const Ranking = () => {
   );
 };
 
-export const getServerSideProps = wrapper.getServerSideProps(async (context) => {
-  console.log('------------------------------------');
-  console.log("SSR start");
-  console.log('------------------------------------');
-  const cookie = context.req ? context.req.headers.cookie : '';
-  axios.defaults.headers.common.Authorization = '';
-  let token = '';
-  // if (context.req && cookie) {
-  //   if (cookie.indexOf(';') !== -1) {
-  //     const index = cookie.indexOf('RefreshToken');
-  //     token = cookie.slice(index + 13, cookie.indexOf(';', index));
-  //   } else {
-  //     token = cookie.slice(13);
-  //   }
-  //   if (token) {
-  //     axios.defaults.headers.common.Authorization = `Bearer ${token}`;
-  //     context.store.dispatch({ type: LOAD_MY_INFO_REQUEST });
-  //   }
-  // }
-  context.store.dispatch({ type: LOAD_LIST_REQUEST });
-  context.store.dispatch(END);
-  console.log('------------------------------------');
-  console.log("SSR end");
-  console.log('------------------------------------');
-  await context.store.sagaTask.toPromise();
-});
+// export const getServerSideProps = wrapper.getServerSideProps(async (context) => {
+//   console.log('------------------------------------');
+//   console.log("SSR start");
+//   console.log('------------------------------------');
+//   const cookie = context.req ? context.req.headers.cookie : '';
+//   axios.defaults.headers.common.Authorization = '';
+//   let token = '';
+//   // if (context.req && cookie) {
+//   //   if (cookie.indexOf(';') !== -1) {
+//   //     const index = cookie.indexOf('RefreshToken');
+//   //     token = cookie.slice(index + 13, cookie.indexOf(';', index));
+//   //   } else {
+//   //     token = cookie.slice(13);
+//   //   }
+//   //   if (token) {
+//   //     axios.defaults.headers.common.Authorization = `Bearer ${token}`;
+//   //     context.store.dispatch({ type: LOAD_MY_INFO_REQUEST });
+//   //   }
+//   // }
+//   context.store.dispatch({ type: LOAD_LIST_REQUEST });
+//   context.store.dispatch(END);
+//   console.log('------------------------------------');
+//   console.log("SSR end");
+//   console.log('------------------------------------');
+//   await context.store.sagaTask.toPromise();
+// });
 
 export default Ranking;
