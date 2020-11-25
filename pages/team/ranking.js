94a3b66ccc4307feb16a65a53e97cc4a1cbdf862
dnrlwo <dnrlwo11@gmail.com>
2020-11-25 17:15:44 +0900

    fix: back to before test

--- pages/team/ranking.js
+++ pages/team/ranking.js
@@ -11,25 +11,7 @@ import wrapper from '../../store/configureStore';
 import { LOAD_MY_INFO_REQUEST } from '../../reducers/user';
 
 const Ranking = () => {
-  // const { rankingList, isLoading } = useSelector((state) => state.team);
-  const rankingList = [
-    {
-      title: '잔디FC',
-      rank: 1,
-      location: '서울',
-      occupation: 3,
-      recruit: 'Y',
-      leader: 'everest88',
-    },
-    {
-      title: 'FC벌',
-      rank: 2,
-      location: '서울',
-      occupation: 1,
-      recruit: 'N',
-      leader: 'leese',
-    },
-  ];
+  const { rankingList } = useSelector((state) => state.team);
   return (
     <AppLayout>
       <Row>
@@ -52,31 +34,25 @@ const Ranking = () => {
   );
 };
 
-// export const getServerSideProps = wrapper.getServerSideProps(async (context) => {
-//   console.log('------------------------------------');
-//   console.log("SSR start");
-//   console.log('------------------------------------');
-//   const cookie = context.req ? context.req.headers.cookie : '';
-//   axios.defaults.headers.common.Authorization = '';
-//   let token = '';
-//   // if (context.req && cookie) {
-//   //   if (cookie.indexOf(';') !== -1) {
-//   //     const index = cookie.indexOf('RefreshToken');
-//   //     token = cookie.slice(index + 13, cookie.indexOf(';', index));
-//   //   } else {
-//   //     token = cookie.slice(13);
-//   //   }
-//   //   if (token) {
-//   //     axios.defaults.headers.common.Authorization = `Bearer ${token}`;
-//   //     context.store.dispatch({ type: LOAD_MY_INFO_REQUEST });
-//   //   }
-//   // }
-//   context.store.dispatch({ type: LOAD_LIST_REQUEST });
-//   context.store.dispatch(END);
-//   console.log('------------------------------------');
-//   console.log("SSR end");
-//   console.log('------------------------------------');
-//   await context.store.sagaTask.toPromise();
-// });
+export const getServerSideProps = wrapper.getServerSideProps(async (context) => {
+  const cookie = context.req ? context.req.headers.cookie : '';
+  axios.defaults.headers.common.Authorization = '';
+  let token = '';
+  if (context.req && cookie) {
+    if (cookie.indexOf(';') !== -1) {
+      const index = cookie.indexOf('RefreshToken');
+      token = cookie.slice(index + 13, cookie.indexOf(';', index));
+    } else {
+      token = cookie.slice(13);
+    }
+    if (token) {
+      axios.defaults.headers.common.Authorization = `Bearer ${token}`;
+      context.store.dispatch({ type: LOAD_MY_INFO_REQUEST });
+    }
+  }
+  context.store.dispatch({ type: LOAD_LIST_REQUEST });
+  context.store.dispatch(END);
+  await context.store.sagaTask.toPromise();
+});
 
 export default Ranking;
