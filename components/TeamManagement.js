14436aadb042f9c9f325882df4b1954793133e95
dnrlwo <dnrlwo11@gmail.com>
2020-11-28 13:46:32 +0900

    fix: useselector

--- components/TeamManagement.js
+++ components/TeamManagement.js
@@ -1,6 +1,6 @@
 import React, { useCallback, useState } from 'react';
 import PropTypes from 'prop-types';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 import { Button, Drawer, Tabs } from 'antd';
 import { ReloadOutlined } from '@ant-design/icons';
