696d3157ac388246f737cb599b089e191f7ddce5
dnrlwo <dnrlwo11@gmail.com>
2020-12-01 15:04:48 +0900

    fix: file check

--- util/imageUploader.js
+++ util/imageUploader.js
@@ -3,6 +3,9 @@ import { post } from 'axios';
 const imageUploader = (url, file, token) => {
   const formdata = new FormData();
   formdata.append('image', file);
+  console.log('------------------------------------');
+  console.log(file, formdata);
+  console.log('------------------------------------');
   const config = {
     headers: { 'content-type': 'multipart/form-data', Authorization: `Bearer ${token}` },
   };
