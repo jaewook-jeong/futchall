c29dc2fbbcc89be46e5c39ccb6fb03170c742beb
dnrlwo <dnrlwo11@gmail.com>
2020-12-01 15:47:09 +0900

    fix: imageuploader

--- util/imageUploader.js
+++ util/imageUploader.js
@@ -3,11 +3,8 @@ import { post } from 'axios';
 const imageUploader = (url, file, token) => {
   const formdata = new FormData();
   formdata.append('image', file);
-  console.log('------------------------------------');
-  console.log(file, formdata);
-  console.log('------------------------------------');
   const config = {
-    headers: { 'content-type': 'multipart/form-data', Authorization: `Bearer ${token}`, 'Access-Control-Allow-Origin': "*" },
+    headers: { 'content-type': 'multipart/form-data', Authorization: `Bearer ${token}`},
   };
   return (
     post(url, formdata, config)
