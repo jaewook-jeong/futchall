0f921b8d6ee4eb4d69058dd5b088daa3525731ed
dnrlwo <dnrlwo11@gmail.com>
2020-12-03 14:02:03 +0900

    fix: _app meta data

--- pages/_app.js
+++ pages/_app.js
@@ -8,7 +8,8 @@ import wrapper from '../store/configureStore';
 const FutChall = ({ Component, pageProps }) => (
   <>
     <Head>
-      <title>FutChall</title>
+      <title>FutChall: 팀, 구장, 랭킹을 모두 한 곳에서</title>
+      <meta name="description" content="풋챌과 함께 풋살팀을 생성하고 지역 구장을 등록하여 구장을 점령하세요! 많은 구장을 점령하여 순위를 올려보세요!" />
       <meta charSet="utf-8" />
       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/antd/4.6.2/antd.min.css" />
       <link rel="icon" type="image/png" href={`/favicon.png`} />
