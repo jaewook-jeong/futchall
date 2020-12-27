29e664671c52ee335bad915edb21b809efd61404
dnrlwo <dnrlwo11@gmail.com>
2020-12-27 13:46:00 +0900

    fix: sh file

--- blue-green.sh
+++ blue-green.sh
@@ -2,20 +2,20 @@
 
 DOCKER_APP_NAME=futchall_nextjs
 
-EXIST_BLUE=$(docker-compose -p ${DOCKER_APP_NAME}-blue -f docker-compose.blue.yml ps | grep Up)
+EXIST_BLUE=$(docker-compose -p ${DOCKER_APP_NAME}-blue_nextjs_1 -f docker-compose.blue.yml ps | grep Up)
 
 if [ -z "$EXIST_BLUE" ]; then
 	echo "blue up"
-	docker-compose -p ${DOCKER_APP_NAME}-blue -f docker-compose.blue.yml up -d
+	docker-compose -p ${DOCKER_APP_NAME}-blue_nextjs_1 -f docker-compose.blue.yml up -d
 
 	sleep 10
 
-	docker-compose -p ${DOCKER_APP_NAME}-green -f docker-compose.green.yml down
+	docker-compose -p ${DOCKER_APP_NAME}-green_nextjs_1 -f docker-compose.green.yml down
 else
 	echo "green up"
-	docker-compose -p ${DOCKER_APP_NAME}-green -f docker-compose.green.yml up -d
+	docker-compose -p ${DOCKER_APP_NAME}-green_nextjs_1 -f docker-compose.green.yml up -d
 
 	sleep 10
 
-	docker-compose -p ${DOCKER_APP_NAME}-blue -f docker-compose.blue.yml down
+	docker-compose -p ${DOCKER_APP_NAME}-blue_nextjs_1 -f docker-compose.blue.yml down
 fi
