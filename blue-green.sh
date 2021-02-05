210414377570c3e853331a370f0dee028a3a84b7
dnrlwo <dnrlwo11@gmail.com>
2021-02-05 19:33:13 +0900

    build: delete option

--- blue-green.sh
+++ blue-green.sh
@@ -6,14 +6,14 @@ EXIST_BLUE=$(docker-compose -p ${DOCKER_APP_NAME}-blue -f docker-compose.blue.ym
 
 if [ -z "$EXIST_BLUE" ]; then
 	echo "blue up"
-	docker-compose -p ${DOCKER_APP_NAME}-blue -f docker-compose.blue.yml up -d --always-recreate-deps
+	docker-compose -p ${DOCKER_APP_NAME}-blue -f docker-compose.blue.yml up -d
 
 	sleep 10
 
 	docker-compose -p ${DOCKER_APP_NAME}-green -f docker-compose.green.yml down
 else
 	echo "green up"
-	docker-compose -p ${DOCKER_APP_NAME}-green -f docker-compose.green.yml up -d --always-recreate-deps
+	docker-compose -p ${DOCKER_APP_NAME}-green -f docker-compose.green.yml up -d
 
 	sleep 10
 
